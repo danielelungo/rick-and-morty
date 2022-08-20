9893dc08f3d2e798aedea9062a92caae6f4dd8df
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-20 13:30:08 +0200

    change usebackListener to tsx

--- src/Hooks/useBackListener.tsx
+++ src/Hooks/useBackListener.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useContext } from "react";
 import { UNSAFE_NavigationContext } from "react-router-dom";
+import { History, Update } from "history";
 
-const useBackListener = (callback) => {
-  const navigator = useContext(UNSAFE_NavigationContext).navigator;
+const useBackListener = (callback: (...args: any) => void) => {
+  const navigator = useContext(UNSAFE_NavigationContext).navigator as History;
 
   useEffect(() => {
-    const listener = ({ location, action }) => {
+    const listener = ({ location, action }: Update) => {
       if (action === "POP") {
         callback({ location, action });
       }
