b78008d065303e8197208bf787755c712e4c4db0
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-20 15:53:52 +0200

    add styling to chart.tsx

--- src/Components/char/char.tsx
+++ src/Components/char/char.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import { Character } from "../../Interfaces/types";
 
 interface TableProps {
@@ -15,23 +16,60 @@ const Char: React.FC<TableProps> = ({ data, page }) => {
   };
 
   return (
-    <div>
+    <>
       {data?.length ? (
         <div>
           {data?.map((item: Character) => (
-            <div key={item?.id} onClick={() => openCharacter(item.id)}>
-              <span>{item.id}</span>
-              <span>{item?.name}</span>
-              <span>{item?.status}</span>
-              <span>{item?.species}</span>
-            </div>
+            <Container key={item?.id} onClick={() => openCharacter(item.id)}>
+              <Name>{item?.name}</Name>
+              <Status color={item?.status}>{item?.status}</Status>
+              <Species>{item?.species}</Species>
+            </Container>
           ))}
         </div>
       ) : (
         "There is no data"
       )}
-    </div>
+    </>
   );
 };
 
 export default Char;
+
+const Container = styled.div`
+  cursor: pointer;
+  background-color: #f4f4f4;
+  display: flex;
+  justify-content: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  border-width: 3px;
+  border-color: black;
+  border-style: solid;
+  margin: 15px;
+  border-radius: 10px;
+  @media only screen and (min-width: 900px) {
+    margin-left: 200px;
+    margin-right: 200px;
+  }
+`;
+
+const Name = styled.div`
+  flex: 2;
+  align-self: center;
+  font-weight: 800;
+`;
+const Status = styled.div`
+  flex: 1;
+  background-color: ${({ color }) =>
+    color === "Alive" ? "#8AC75F " : color === "Dead" ? "#C23C41" : "#A1CBDC"};
+  align-self: center;
+  border-radius: 50px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+const Species = styled.div`
+  flex: 1;
+  align-self: center;
+  font-weight: 600;
+`;
