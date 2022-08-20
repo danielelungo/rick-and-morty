a30d554638e0ba241ce20577a8cc01d2e3fad098
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-20 12:26:32 +0200

    functional home page

---
+++ src/Pages/home/home.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Char from "../../Components/char/char";
+import useAllChars from "../../Hooks/useAllChars";
+import usePagination from "../../Hooks/usePagination";
+
+type PageId = {
+  pageId: string;
+};
+
+function Home() {
+  const location = useLocation();
+  const { pageId } = location?.state as PageId;
+
+  const { page, nextPageHandler, prevPageHandler } = usePagination(
+    pageId ? Number(pageId) : 1,
+  );
+
+  const { data, error, loading, numPages } = useAllChars(page);
+
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        {error ? (
+          <span>{error}</span>
+        ) : loading ? (
+          <span>loading</span>
+        ) : (
+          data && <Char data={data} page={page} />
+        )}
+      </div>
+      <div>
+        <button disabled={page === 1} onClick={prevPageHandler}>
+          prev
+        </button>
+        <span>Page: {page}</span>
+        <button disabled={page === numPages} onClick={nextPageHandler}>
+          next
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
