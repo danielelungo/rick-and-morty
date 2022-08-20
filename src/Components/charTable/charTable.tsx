0d4ff69d4dafda6eea02b81abc968489f6b24a38
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-20 12:33:41 +0200

    functional characterTable Component

---
+++ src/Components/charTable/charTable.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type EpisodesType = {
+  id: number;
+  name: string;
+  date: string;
+};
+
+interface CharTableProps {
+  data: EpisodesType[];
+}
+
+const CharTable: React.FC<CharTableProps> = ({ data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((item: EpisodesType) => (
+          <tr key={item?.id}>
+            <td>{item.id}</td>
+            <td>{item?.name}</td>
+            <td>{item?.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CharTable;
