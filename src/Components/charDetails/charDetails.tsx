f9d324587aefb83e1d8c840a9e151b7fbd9e6323
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-20 12:33:24 +0200

    functional characterDetails Component

---
+++ src/Components/charDetails/charDetails.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface CharDetailProps {
+  list?: boolean;
+  data: string | number;
+  title: string;
+  dataTestid: string;
+
+  className?: React.HTMLAttributes<HTMLSpanElement> | string;
+}
+
+const CharDetail: React.FC<CharDetailProps> = ({
+  list = false,
+  data,
+  title,
+  className = "",
+  dataTestid,
+}) => {
+  return (
+    <>
+      {list ? (
+        <li data-testid={dataTestid}>
+          {title} {data}
+        </li>
+      ) : (
+        <span data-testid={dataTestid}>
+          {title} {data}
+        </span>
+      )}
+    </>
+  );
+};
+
+export default CharDetail;
