34727a640d4404038ecdf1c400cea49d2ed85f32
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-20 18:18:35 +0200

    add button styling

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Oval } from "react-loader-spinner";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
 import Char from "../../Components/char/char";
 import useAllChars from "../../Hooks/useAllChars";
 import usePagination from "../../Hooks/usePagination";
@@ -22,28 +23,48 @@ function Home() {
   return (
     <>
       <h1>Home</h1>
-      <div>
-        {error ? (
-          <h1>{error}</h1>
-        ) : loading ? (
-          <Oval wrapperClass="loader" height={150} width={150} />
-        ) : (
-          data && (
-            <div>
-              <Char data={data} page={page} />
-              <button disabled={page === 1} onClick={prevPageHandler}>
+      {error ? (
+        <h1>{error}</h1>
+      ) : loading ? (
+        <Oval wrapperClass="loader" height={150} width={150} />
+      ) : (
+        data && (
+          <div>
+            <Char data={data} page={page} />
+            <ButtonContainer>
+              <Button disabled={page === 1} onClick={prevPageHandler}>
                 prev
-              </button>
-              <span>Page: {page}</span>
-              <button disabled={page === numPages} onClick={nextPageHandler}>
+              </Button>
+              <PageNumber>Page: {page}</PageNumber>
+              <Button disabled={page === numPages} onClick={nextPageHandler}>
                 next
-              </button>
-            </div>
-          )
-        )}
-      </div>
+              </Button>
+            </ButtonContainer>
+          </div>
+        )
+      )}
     </>
   );
 }
 
 export default Home;
+
+const ButtonContainer = styled.div`
+  margin: 20px;
+`;
+
+const Button = styled.button`
+  cursor: ${({ disabled }) => (disabled ? "default" : "pointer")};
+  background-color: ${({ disabled }) => (disabled ? "#A09FA4" : "#F44250")};
+  color: white;
+  margin-left: 20px;
+  margin-right: 20px;
+  border-radius: 10px;
+  font-size: 20px;
+  padding: 10px;
+`;
+
+const PageNumber = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+`;
