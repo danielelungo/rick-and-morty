dc75a6ce341a8a139982bc698f859b27fc11ba11
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-22 01:15:11 +0200

    add correct charTable testings when rendering

--- src/Components/charTable/charTable.test.tsx
+++ src/Components/charTable/charTable.test.tsx
@@ -1,3 +1,4 @@
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import renderer from "react-test-renderer";
 import { episodeData } from "../../__mocks__/episodeData";
@@ -7,5 +8,14 @@ describe("CharTable component", () => {
   test("renders CharTable Component", () => {
     const tree = renderer.create(<CharTable data={episodeData} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  test("renders correct Name, Episode Numer and Date text", () => {
+    render(<CharTable data={episodeData} />);
+    const episodeNumberText = screen.getByText(episodeData[0].episode);
+    const episodeNameText = screen.getByText(episodeData[0].name);
+    const releaseDateText = screen.getByText(episodeData[0].date);
+    expect(episodeNumberText).toBeInTheDocument();
+    expect(episodeNameText).toBeInTheDocument();
+    expect(releaseDateText).toBeInTheDocument();
   });
 });
