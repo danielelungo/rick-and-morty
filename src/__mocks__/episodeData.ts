d0d09c99c9a0860639dbcdc51addf914835ee1dc
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-22 00:51:17 +0200

    add episode to mock

--- src/__mocks__/episodeData.ts
+++ src/__mocks__/episodeData.ts
@@ -1,4 +1,4 @@
 export const episodeData = [
-  { date: "December 2, 2013", id: 1, name: "Pilot" },
-  { date: "December 9, 2013", id: 2, name: "Lawnmower Dog" },
+  { date: "December 2, 2013", id: 1, name: "Pilot", episode: "S01E01" },
+  { date: "December 9, 2013", id: 2, name: "Lawnmower Dog", episode: "S03E03" },
 ];
