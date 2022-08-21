a91caa331882c5f08d0669285f677a7cc4005fa2
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-22 00:59:15 +0200

    add useCharLocation testing

---
+++ src/Hooks/useCharLocation/useCharLocation.test.tsx
@@ -0,0 +1,43 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { locationDetails } from "../../__mocks__/locationDetails";
+import useCharLocation from "./useCharLocation";
+
+describe("useChatLocation", () => {
+  test("useChatLocation performs GET request", async () => {
+    const mock = new MockAdapter(axios);
+    const url = `https://rickandmortyapi.com/api/location/20`;
+    const mockData = locationDetails;
+
+    mock.onGet(url).reply(200, mockData);
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useCharLocation(url),
+    );
+
+    expect(result.current.locationData).toEqual(undefined);
+
+    await waitForNextUpdate();
+
+    expect(result.current.locationData).toEqual(locationDetails);
+  });
+  test("useChatLocation should display error ", async () => {
+    const mock = new MockAdapter(axios);
+    const url = `https://rickandmortyapi.com/api/location/20`;
+    const mockData = locationDetails;
+
+    mock.onGet(url).reply(404, mockData);
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useCharLocation(url),
+    );
+
+    expect(result.current.errorLocation).toEqual("");
+
+    await waitForNextUpdate();
+
+    expect(result.current.errorLocation).toEqual("Can't fetch data");
+  });
+});
