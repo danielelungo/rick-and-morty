b79986a1e19ddc26d72e6817dfa344fff6a3e023
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-22 00:54:03 +0200

    add char testing

---
+++ src/Components/char/char.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Char from "./char";
+import { BrowserRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
+import { data } from "../../__mocks__/charData";
+import { render, screen } from "@testing-library/react";
+
+describe("Char component", () => {
+  test("renders Char Component", () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Char data={data} page={1} />
+        </BrowserRouter>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("renders correct Name, Status and Species text", () => {
+    render(
+      <BrowserRouter>
+        <Char data={data} page={1} />
+      </BrowserRouter>,
+    );
+    const nameText = screen.getByText(data[0].name);
+    const statusText = screen.getByText(data[0].status);
+    const speciesText = screen.getByText(data[0].species);
+    expect(nameText).toBeInTheDocument();
+    expect(statusText).toBeInTheDocument();
+    expect(speciesText).toBeInTheDocument();
+  });
+  test("renders error if data.length is 0", () => {
+    render(
+      <BrowserRouter>
+        <Char data={[]} page={1} />
+      </BrowserRouter>,
+    );
+    const errorText = screen.getByText(/There is no data/);
+    expect(errorText).toBeInTheDocument();
+  });
+});
